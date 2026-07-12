6b7f77dac8a0c16720c9a60afe9282d83644ffad
Doston <nnuraliyev51@gmail.com>
2026-07-12 16:04:28 +0500

    chore: enable basic ssl for local network camera access

--- vite.config.ts
+++ vite.config.ts
@@ -1,11 +1,13 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import svgr from "vite-plugin-svgr";
+import basicSsl from "@vitejs/plugin-basic-ssl";
 import path from "path";
 
 export default defineConfig({
   plugins: [
     react(),
+    basicSsl(),
     svgr({
       svgrOptions: {
         icon: true,
